c99a763796e25c57a63f4426f99fe8aa5d2986fa
Jayamvr007 <jayamverma@gmail.com>
2026-03-29 18:15:06 +0530

    Initial Commit

---
+++ Haat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Haat
+//
+//  Created by Jayam Verma on 29/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Haat/HaatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaatApp.swift
+//  Haat
+//
+//  Created by Jayam Verma on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HaatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
